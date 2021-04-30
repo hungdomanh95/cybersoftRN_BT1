db6dc0a48c09ccfc65aee6ed4dd927e5e60bdba1
DoHung <dohung@hungKdev.local>
2021-04-30 22:54:13 +0700

    upcode buoi3

--- App.js
+++ App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import RenderIcon from './src/components/Buoi5/RenderIcon';
 import RenderWidthState from './src/components/Buoi5/RenderWidthState';
-// import Buoi3 from './src/components/Buoi3';
+import Buoi3 from './src/components/Buoi3';
 import Touched from './src/components/Buoi5/Touched';
 const App = () => {
   return (
     <View style={styles.container}>
-      {/* <Buoi3 /> */}
+      <Buoi3 />
       {/* <Touched /> */}
       {/* <RenderWidthState /> */}
-      <RenderIcon />
+      {/* <RenderIcon /> */}
     </View>
   );
 };
--- src/components/Buoi3/index.js
+++ src/components/Buoi3/index.js
@@ -9,24 +9,19 @@ const Buoi3 = () => {
   return (
     <View style={styles.container}>
       <View style={styles.boxTop}>
-        {/* <ItemTop />
-          <ItemTop /> */}
-
-        <Text style={styles.textAvatar}>
-          {isShow ? (
-            <Image source={require('../../img/a.jpg')} style={styles.images} />
-          ) : (
-            <Text>Image Hidden</Text>
-          )}
-        </Text>
+        <View style={styles.itemTop} />
+        <View style={styles.itemTop} />
       </View>
       <View style={styles.line} />
       <View style={styles.boxBottom}>
-        <ItemBottom />
-        <ItemBottom />
-        <ItemBottom />
-        <ItemBottom />
-        {/* <BoxItem /> */}
+        <View style={styles.boxItem}>
+          <View style={styles.itemBottom} />
+          <View style={styles.itemBottom} />
+        </View>
+        <View style={styles.boxItem}>
+          <View style={styles.itemBottom} />
+          <View style={styles.itemBottom} />
+        </View>
       </View>
     </View>
   );
@@ -47,8 +42,6 @@ const styles = StyleSheet.create({
     flex: 20,
     borderRadius: 20,
     paddingTop: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   boxBottom: {
     backgroundColor: '#c8c8fA',
@@ -56,7 +49,6 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     flexDirection: 'row',
     paddingBottom: '10%',
-    flexWrap: 'wrap',
   },
   itemTop: {
     backgroundColor: '#ffffc2',
@@ -64,8 +56,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 20,
     marginVertical: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   itemBottom: {
     backgroundColor: '#4f82c0',
@@ -73,19 +63,5 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     margin: '10%',
   },
-  images: {
-    width: 200,
-    height: 200,
-    borderRadius: 200,
-    borderWidth: 5,
-    borderColor: 'white',
-  },
-  textAvatar: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: 'white',
-    marginTop: 20,
-  },
 });
-
 export default Buoi3;
