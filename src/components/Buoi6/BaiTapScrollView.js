c965bb2b16a630b270630e2c59dcdfcab3e22983
DoHung <dohung@hungKdev.local>
2021-05-04 09:44:07 +0700

    chia component

--- src/components/Buoi6/BaiTapScrollView.js
+++ src/components/Buoi6/BaiTapScrollView.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, ScrollView} from 'react-native';
+import {View, StyleSheet, ScrollView} from 'react-native';
+import ItemScrollView from './ItemScrollView';
 
 export default class BaiTapScrollView extends Component {
   arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
@@ -7,19 +8,8 @@ export default class BaiTapScrollView extends Component {
     return (
       <View style={styles.container}>
         <ScrollView>
-          {this.arr.map(item => (
-            <View
-              style={[
-                styles.item,
-                {
-                  backgroundColor: `#${Math.floor(
-                    Math.random() * 16777215,
-                  ).toString(16)}`,
-                },
-              ]}
-              key={item}>
-              <Text>item {item}</Text>
-            </View>
+          {this.arr.map((item, index) => (
+            <ItemScrollView item={item} key={index} />
           ))}
         </ScrollView>
       </View>
@@ -31,12 +21,5 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingVertical: 50,
     paddingHorizontal: 20,
-  },
-  item: {
-    height: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: 10,
-    borderRadius: 10,
   },
 });
